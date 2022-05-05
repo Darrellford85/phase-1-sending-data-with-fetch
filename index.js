2b8f6bbedeeae128ef0d7b5eb6514de74c9d7c7e
Darrell Ford <Darrellford85@gmail.com>
2022-05-05 06:35:27 -0700

    Finally Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// Add your code here- https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch//
+const para = document.querySelector('#append')
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+
+     })
+     .then(response => response.json())
+     .then(users=>{ const newId = document.createTextNode(users.id)
+         return para.appendChild(newId)}
+         )
+      .catch(error=>para.textContent += error.message)
+    }
